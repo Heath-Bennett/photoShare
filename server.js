98393fa7f9fbf3982447d1c17d602d5af83e3096
Heath Bennett <heathben418@gmail.com>
2023-01-24 18:50:31 -0500

    added ejs to the project

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = new express();
 const port = 8080;
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.listen(port, ()=>{
     console.log(`Serving photo app on http://localhost:${port}`);
+});
+
+app.get('/', (request, response) => {
+    response.render('index');
 });
